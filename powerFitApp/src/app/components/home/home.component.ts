ecbe1de78d02be5e715cddcf4eb02a07b6ebc45a
Leandro Gonçalves <159172886+lleandrogon@users.noreply.github.com>
2024-11-12 22:21:07 -0300

    Adicionando animações ao home

--- powerFitApp/src/app/components/home/home.component.ts
+++ powerFitApp/src/app/components/home/home.component.ts
@@ -1,13 +1,34 @@
 import { Component } from '@angular/core';
 import { GlobalService } from '../../services/global.service';
 import { RouterModule } from '@angular/router';
+import { trigger, transition, style, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [RouterModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
+  animations: [
+    trigger('buttonAnimation', [
+      transition(':enter', [
+        style({ transform: 'translateY(50px)', opacity: 0 }),
+        animate('1000ms ease-out', style({ transform: 'translateY(0)', opacity: 1 }))
+      ])
+    ]),
+    trigger('logoAnimation', [
+      transition(':enter', [
+        style({ transform: 'scale(0.5)', opacity: 0 }),
+        animate('1300ms ease-out', style({ transform: 'scale(1)', opacity: 1 }))
+      ])
+    ]),
+    trigger('textAnimation', [
+      transition(':enter', [
+        style({ opacity: 0 }),
+        animate('1500ms ease-out', style({ opacity: 1 }))
+      ])
+    ])
+  ]
 })
 export class HomeComponent {
   constructor (public globalService: GlobalService) {}
